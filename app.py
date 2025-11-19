2e77620deed509652e36f403530a28656e859d58
Venkteshsha <venkteshsharma11@gmail.com>
2025-11-19 18:36:07 +0000

    new

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask,request,jsonify,render_template
+from business import get_data
+
+app=Flask(__name__)
+
+@app.route('/')
+def hello_world():
+        return render_template('index.html')
+
+@app.route('/api',methods=['GET'])
+def api():
+        data=get_data()
+        data={'data':data}
+        return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8000)
